1c35a3dd039dded5d575099991030f3334ea3f9d
Md Mashrurul Haque <59013281+iAnsari123@users.noreply.github.com>
2023-12-08 21:11:18 +0600

    third commit

--- app.py
+++ app.py
@@ -6,24 +6,26 @@ import joblib
 app = Flask(__name__)
 model = joblib.load("./models/model.pkl")
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route('/predict',methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-
     int_features = [int(float(x)) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
+    return render_template(
+        "index.html", prediction_text="Forecasted Sales: $ {}".format(output)
+    )
 
-    return render_template('index.html', prediction_text='Forecasted Sales: $ {}'.format(output))
 
-@app.route('/results',methods=['POST'])
+@app.route("/results", methods=["POST"])
 def results():
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
@@ -31,5 +33,6 @@ def results():
     output = prediction[0]
     return jsonify(output)
 
+
 if __name__ == "__main__":
-    app.run(debug=False, port=8000)
+    app.run()
